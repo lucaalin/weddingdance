85b0be66f3a920e93575e29f3db39e37a21ad2a7
Alin Luca <alinluca@smallfootprint.com>
2016-01-26 22:32:50 +0200

    Form

--- js/controllers/ContactController.js
+++ js/controllers/ContactController.js
@@ -5,7 +5,31 @@ weddingApp.controller('ContactController',
 
         $scope.pagecontent = {
             title: 'Contact',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            requiredmessage: 'Va rugam sa completati toate campurile de mai sus.'
+        };
+
+
+        $scope.sendUserData = function(user) {
+
+        }
+
+        $scope.user = {
+            firstName: '',
+            lastName: '',
+            phoneNo: '',
+            emailAddress: '',
+            interests: [
+                {
+                    title: ''
+                },
+                {
+                    title: ''
+                }
+            ],
+            eventDate: '',
+            eventLocation: '',
+            message: ''
         }
 
     }
