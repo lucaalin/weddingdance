d7225102679550659ad664fdbfdb35b686e4f646
Alin Luca <alinluca@smallfootprint.com>
2016-03-03 11:06:23 +0200

    Contact form fixed

--- contact.php
+++ contact.php
@@ -1,16 +1,19 @@
 <?php
-    $receiver="contact@alinluca.com";
-	// the message
-	$subject = $_POST["firstName"]." ". $_POST["lastName"]." messaged you !";
-    $msg="Contact Details: Name ".$_POST["firstName"]." ".$_POST["lastName"]."\n".
-	"email: ".$_POST["emailAddress"]."\n".
-	"phone number: ".$_POST["phoneNo"]."\n".
-	"interest: ".$_POST["interest"]."\n".
-	"eventDate: ".$_POST["eventDate"]."\n".
-	"location: ".$_POST["location"]."\n".
-	$_POST["message"];
-	// use wordwrap() if lines are longer than 70 characters
-	$msg = wordwrap($msg,70);
-// send email
-mail($receiver,$subject,$msg);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
+    $_POST = json_decode(file_get_contents('php://input'), true);
+
+
+        $receiver = "dans@weddingdance.ro";
+	$subject = $_POST['firstName'] . " " . $_POST['lastName'] . " ti-a trimis un mesaj pe site";
+        $msg = "Persoana : " .
+        $_POST['firstName'] . " " .
+        $_POST['lastName'] . "\n\n" .
+	"adresa email: " . $_POST['emailAddress'] . "\n\n" .
+	"telefon: ".$_POST['phoneNo'] . "\n\n" .
+	"optiunea selectata (1 - dansul mirilor, 2- cursuri de dans): " . $_POST['interest'] . "\n\n" .
+	"data eveniment: " . $_POST['eventDate'] . "\n\n" .
+	"locatia: " . $_POST['eventLocation'] . "\n\n" .
+	"mesaj: " . $_POST['message'];
+        mail($receiver,$subject,$msg);
 ?>
